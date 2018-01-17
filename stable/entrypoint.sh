c5b99882362a98ceac164cdfdd525d3724d6024d
Jam Risser <jam@jamrizzi.com>
2018-01-17 01:16:19 -0600

    Added initpop

--- stable/entrypoint.sh
+++ stable/entrypoint.sh
@@ -2,5 +2,10 @@
 
 cp /usr/share/zoneinfo/$TZ /etc/localtime
 echo $TZ > /etc/timezone
-envstamp /var/www/html/config
+if [ "$MANUEL_SETUP" != "true" ]; then
+  initpop /etc/initpop.yml
+  rm -r /var/www/html/config
+  mv /var/www/html/_config /var/www/html/config
+  envstamp /var/www/html/config
+fi
 exec /usr/bin/supervisord -c /etc/supervisord.conf
