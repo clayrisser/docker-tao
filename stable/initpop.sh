a10c31730f24de662d1ab182571bb52900958f5a
Jam Risser <jam@jamrizzi.com>
2018-01-18 03:40:01 -0600

    Use port 8080

--- stable/initpop.sh
+++ stable/initpop.sh
@@ -4,7 +4,7 @@ if [ "$MANUEL_SETUP" == "true" ]; then
   rm -r /var/www/html/_config
 else
   if [ "$(mysql -h${DATABASE_HOST} -u${DATABASE_USER} -p${DATABASE_PASSWORD} -e "use ${DATABASE_NAME}; show tables;" | grep statements)" = "" ]; then
-    until $(curl --output /dev/null --silent --head --fail http://localhost/tao/install); do
+    until $(curl --output /dev/null --silent --head --fail http://localhost:8080/tao/install); do
       sleep 5
     done
     initpop /etc/initpop.yml
